f499690c9757f075ef989ed76fa6d1f833dd2190
David Cherriere <david.cherriere@oscaro.com>
2020-07-16 10:01:56 +0200

    lot of feature an stuff :)

--- exporter.py
+++ exporter.py
@@ -9,10 +9,13 @@ import setproctitle
 import logging
 import functools
 import operator
+import re
 from pprint import pprint
 from logging.handlers import RotatingFileHandler
 from prometheus_client import start_http_server, Summary, Gauge
 from elasticsearch import Elasticsearch
+from elasticsearch import exceptions as es_exceptions
+from datetime import datetime
 
 
 def signal_handler(sig, frame):
@@ -26,16 +29,23 @@ def shutdown():
     pass
 
 
-class EsQueryExporter:
+class es_query_exporter:
     def __init__(self, config):
         self.cfg = config
-        self.GaugeDict = {}
-        self.ReqDict = {}
+        self.gauge_dict = {}
+        self.req_dict = {}
         self.logger = logging.getLogger()
 
-        self.prepareLogs()
+        self.__prepare_logs()
 
-    def prepareLogs(self):
+    def __get_export_path(self, export_str):
+        export_path = export_str.split(".")
+        for key in range(len(export_path)):
+            if re.match("[0-9]", export_path[key]):
+                export_path[key] = int(export_path[key])
+        return export_path
+
+    def __prepare_logs(self):
         if "loglevel" in self.cfg["exporter"]:
             self.logger.setLevel(
                 getattr(logging, self.cfg["exporter"]["loglevel"].upper())
@@ -57,7 +67,7 @@ class EsQueryExporter:
             stream_handler.setLevel(logging.DEBUG)
             self.logger.addHandler(stream_handler)
 
-    def getLabelNames(self, source):
+    def __get_label_names(self, source):
         labels = []
         for i in source:
             if "labels" in source[i] and labels == []:
@@ -67,84 +77,104 @@ class EsQueryExporter:
 
         return list(dict.fromkeys(labels))
 
-    def createGauge(self):
-        for Metric in self.cfg["metrics"]:
-            for MetricName, MetricParam in Metric.items():
+    def __create_gauge(self):
+        for metric in self.cfg["metrics"]:
+            for metric_name, metric_param in metric.items():
                 # If there is label in metric :
-                if len(MetricParam["sources"]) > 1:
-                    for source in MetricParam["sources"]:
-                        if MetricName not in self.GaugeDict:
-                            Labels = self.getLabelNames(source)
-                            self.GaugeDict[MetricName] = Gauge(
-                                MetricName,
-                                MetricParam["description"],
-                                labelnames=Labels,
+                if len(metric_param["sources"]) > 1:
+                    for source in metric_param["sources"]:
+                        if metric_name not in self.gauge_dict:
+                            labels = self.__get_label_names(source)
+                            self.gauge_dict[metric_name] = Gauge(
+                                metric_name,
+                                metric_param["description"],
+                                labelnames=labels,
                             )
                             self.logger.info(
-                                "Created labelled metric gauge %s" % (MetricName)
+                                "Created labelled metric gauge %s" % (metric_name)
                             )
                 elif (
-                    len(MetricParam["sources"]) == 1
-                    and MetricName not in self.GaugeDict
+                    len(metric_param["sources"]) == 1
+                    and metric_name not in self.gauge_dict
                 ):
-                    self.GaugeDict[MetricName] = Gauge(
-                        MetricName, MetricParam["description"]
+                    self.gauge_dict[metric_name] = Gauge(
+                        metric_name, metric_param["description"]
                     )
                     self.logger.info(
-                        "Created unlabelled metric gauge %s" % (MetricName)
+                        "Created unlabelled metric gauge %s" % (metric_name)
                     )
 
-    def setLabelledMetric(self, MetricName, SourceList):
-        for source in SourceList:
-            for SourceName, SourceParam in source.items():
-                ExportPath = SourceParam["export"].split(".")
-                self.GaugeDict[MetricName].labels(**SourceParam["labels"]).set(
-                    functools.reduce(
-                        operator.getitem, ExportPath, self.ReqDict[SourceName]
+    def __set_labelled_metric(self, metric_name, source_list):
+        for source in source_list:
+            for source_name, source_param in source.items():
+                export_path = self.__get_export_path(source_param["export"])
+                try:
+                    self.gauge_dict[metric_name].labels(**source_param["labels"]).set(                    
+                        functools.reduce(
+                            operator.getitem, export_path, self.req_dict[source_name]
+                        )
                     )
+                except KeyError as e:
+                    self.logger.error("Unable to export request. Maybe server is currently down or target do not exists")
+                    self.logger.error("    Key error  %s not found" % (e))
+
+    def __set_unlabelled_metric(self, metric_name, source_dict):
+        for source_name, source_param in source_dict.items():
+            export_path = self.__get_export_path(source_param["export"])
+            self.gauge_dict[metric_name].set(
+                functools.reduce(
+                    operator.getitem, export_path, self.req_dict[source_name]
                 )
-
-    def setUnlabelledMetric(self, MetricName, SourceDict):
-        for SourceName, SourceParam in SourceDict.items():
-            ExportPath = SourceParam["export"].split(".")
-            self.GaugeDict[MetricName].set(
-                functools.reduce(operator.getitem, ExportPath, self.ReqDict[SourceName])
             )
 
-    def exportMetrics(self):
-        for Metric in self.cfg["metrics"]:
-            for MetricName, MetricParam in Metric.items():
-                if len(MetricParam["sources"]) > 1:
-                    self.setLabelledMetric(MetricName, MetricParam["sources"])
-                elif len(MetricParam["sources"]) == 1:
-                    self.setUnlabelledMetric(MetricName, MetricParam["sources"][0])
+    def __export_metric(self):
+        for metric in self.cfg["metrics"]:
+            for metric_name, metric_param in metric.items():
+                if len(metric_param["sources"]) > 1:
+                    self.__set_labelled_metric(metric_name, metric_param["sources"])
+                elif len(metric_param["sources"]) == 1:
+                    self.__set_unlabelled_metric(metric_name, metric_param["sources"][0])
 
-    def proceedEsQuery(self):
-        for Request in self.cfg["requests"]:
-            for ReqName, ReqParam in Request.items():
-                self.logger.info("Proceeding query %s" % (ReqName))
+    def __proceed_es_query(self):
+        for request in self.cfg["requests"]:
+            for req_name, req_param in request.items():
+                self.logger.info("Proceeding query %s" % (req_name))
                 try:
-                    ReqBody = ReqParam["body"] if "body" in ReqParam else None
-                    es = Elasticsearch(ReqParam["server"], retry_on_timeout=False)
-                    res = getattr(es, ReqParam["action"])(
-                        index=ReqParam["index"], body=ReqBody
+                    req_body = req_param["body"] if "body" in req_param else None
+                    es = Elasticsearch(req_param["server"], retry_on_timeout=False)
+                    res = getattr(es, req_param["action"])(
+                        index=req_param["index"], body=req_body
                     )
+                # if es is a shlagos out of date version lol
+                except (es_exceptions.NotFoundError):
+                    reg = re.compile(r"<(.*)(\{now\/d\{yyyy.MM.dd\}\})>")
+                    cap = reg.match(req_param["index"])
+                    if cap != None:
+                        index_fixed = cap.group(1) + datetime.today().strftime("%Y.%m.%d")
+                        try:
+                            res = getattr(es, req_param["action"])(
+                                index=index_fixed, body=req_body
+                            )
+                        except:
+                            pass
+                    pass
                 except:
-                    self.logger.error("Error : Unable to proceed request")
+                    self.logger.error(
+                        "Error : Unable to proceed request %s" % (req_name)
+                    )
                     res = False
                     pass
+                self.req_dict[req_name] = res
 
-                self.ReqDict[ReqName] = res
-
-    def startHTTPServer(self):
+    def __start_server(self):
         start_http_server(int(self.cfg["exporter"]["port"]))
 
-    def runExporter(self):
-        self.createGauge()
-        self.startHTTPServer()
+    def run_exporter(self):
+        self.__create_gauge()
+        self.__start_server()
         while True:
-            self.proceedEsQuery()
-            self.exportMetrics()
+            self.__proceed_es_query()
+            self.__export_metric()
             time.sleep(int(self.cfg["exporter"]["refresh"]))
 
 
@@ -155,5 +185,5 @@ if __name__ == "__main__":
         os.path.dirname(os.path.realpath(__file__)) + "/config.yaml", "r"
     ) as yamlconf:
         cfg = yaml.load(yamlconf, Loader=yaml.FullLoader)
-    esqe = EsQueryExporter(cfg)
-    esqe.runExporter()
+    esqe = es_query_exporter(cfg)
+    esqe.run_exporter()
